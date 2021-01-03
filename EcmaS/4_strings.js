5ef1da5fabd4aeaefc1ef5b6a7f1601c8822f42f
HanNabu <uniondirfolder@gmail.com>
2021-01-03 15:55:59 +0200

    4

--- EcmaS/4_strings.js
+++ EcmaS/4_strings.js
@@ -1,3 +1,19 @@
 //'' ""
 const title = 'Foo'
-const template=`<h1 style="color: green" >${title}</h1>`
+const isVisible=()=>Math.random()>0.5;
+const template=`
+${isVisible()? `<p>Visible</p>`: ''}
+     "spaces_to_includ"
+<h1 style="color: green" >${title}</h1>
+`
+
+const str = 'Hello';
+console.log(str.startsWith('He'));
+console.log(str.endsWith('lo'));
+console.log(str.includes('llo'));
+console.log(str.repeat(3));
+console.log(str.trim());
+console.log(str.trimEnd());
+console.log(str.trimStart());
+console.log(str.padStart(10));
+console.log(str.padEnd(10));
